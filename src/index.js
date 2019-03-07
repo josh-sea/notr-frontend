b05b92e24b8995178c92d9854fc8c4de86e13a00
josh-sea <joshua.cocciardi@gmail.com>
2019-03-07 15:01:43 -0500

    some changes

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { ActionCableProvider } from 'react-actioncable-provider';
 // const CABLEURL = `ws://localhost:3000/cable`
-// const CABLEURL = `wss://notr-backend.herokuapp.com/cable`
-const CABLEURL = `ws://${window.location.hostname}:3000/cable`
+const CABLEURL = `wss://notr-backend.herokuapp.com/cable`
+// const CABLEURL = `ws://${window.location.hostname}:3000/cable`
 
 ReactDOM.render(
   <ActionCableProvider url={`${CABLEURL}`}>
