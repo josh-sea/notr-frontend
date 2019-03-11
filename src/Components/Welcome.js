f01c2122c3ca26fd1245563e9ad7bdd7e065733a
josh-sea <joshua.cocciardi@gmail.com>
2019-03-11 12:22:21 -0400

    on save only

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -23,12 +23,10 @@ const Welcome = ({handleMenuClickSim}) => (
         <p style={align}>Under Water Basket Weaving</p>
         <br/>
         <p>To the left, your list of your classrooms will appear, if you have any current classes. You can add as many classes as you want and as may notes as you want to each classroom.</p>
-        <p>Once you have created a new classroom, you can fill it with lecture notes! Each note should ideally be a single lecture. But you can do whatever you want.</p>
-        <p>When you create a new classroom you will get an empty note you can edit.</p>
         <p>The menu allows you to create a new classroom or note, to save your changes, delete notes and to logout.</p>
         <p></p>
         <p>To get started, create a new classroom using the top menu (use the 'Start' button to get started).</p>
-        <p>BONUS: You may find this note system to be live. If you view a fellowclassmates note who is currently editing, you will see the changes live.</p>
+        <p>BONUS: If you view a fellowclassmates note who is currently editing, you will see the changes everytime they save their notes and everytime you save your notes they will see it too!</p>
         <p>BONUS BONUS: A wikipedia search has been added to the top right, feel free to use wikipedia to add to your notes!</p>
       </Header>
         <div style={align}><Button onClick={handleMenuClickSim}>Start</Button></div>
@@ -38,3 +36,7 @@ const Welcome = ({handleMenuClickSim}) => (
 );
 
 export default Welcome;
+
+
+// <p>Once you have created a new classroom, you can fill it with lecture notes! Each note should ideally be a single lecture. But you can do whatever you want.</p>
+// <p>When you create a new classroom you will get an empty note you can edit.</p>
